34719bce2b727344d9a91edeb4ce2cc1a940011a
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-08 11:03:02 +0600

    recursion namta modify

--- recursion.py
+++ recursion.py
@@ -10,24 +10,32 @@
 #             print('-----------------------')
 #         namta(n-1)
 
-def print_namta(n, m):
-    if m == 0:
+# def print_namta(n, m):
+#     if m == 0:
+#         return
+#     else:
+#         print("{} * {} = {}".format(m, n, m*n))
+#         print_namta(n, m-1)
+
+def print_namta(n, m, i):
+    if i == m + 1:
         return
     else:
-        print("{} * {} = {}".format(m, n, m*n))
-        print_namta(n, m-1)
+        print("{} * {} = {}".format(i, n, i*n))
+        i += 1
+        print_namta(n, m, i)
 
 
 def namta(n, m):
     if n == 0:
         return
     else:
-        print_namta(n, m)
+        print_namta(n, m, 1)
         if n != 1:
             print('-----------------------')
         namta(n-1, m)
 
 
 n = 3
-m = 12
+m = 10
 namta(n, m)

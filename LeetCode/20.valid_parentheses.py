67b4685c70c4f872015b82162a64648f56517bac
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-22 19:09:53 +0600

    leetcode 20. validparentheses

---
+++ LeetCode/20.valid_parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        hash = {
+            "}": "{",
+            ")": "(",
+            "]": "["
+        }
+
+        for c in s:
+            if c in hash:
+                if stack and stack[-1] == hash[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if len(stack) == 0 else False
---
+++ LeetCode/t.py
@@ -0,0 +1,8 @@
+
+
+def merge_two_list(l1, l2):
+    pass
+
+
+list1 = [1, 2, 4]
+list2 = [1, 3, 4]

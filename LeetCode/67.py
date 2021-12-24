6a57a0660bd3b6123a76d3e3fd6974e44799ba34
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-24 10:19:24 +0600

    leetcode 67

--- LeetCode/67.py
+++ LeetCode/67.py
@@ -1,5 +1,4 @@
 
-
 class Solution:
     def addBinary(self, a, b):
         sum = bin(int(a, 2) + int(b, 2))
@@ -10,6 +9,3 @@ ob = Solution()
 a = "11"
 b = "1"
 print(ob.addBinary(a, b))
-
-
-# print(int(b, 2))
